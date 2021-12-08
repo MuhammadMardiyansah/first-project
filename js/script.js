8b4292aef8127b4c73dd999f2538d78abd405923
MuhammadMardiyansah <mhmmardi22@gmail.com>
2021-12-08 14:42:19 +0700

    first push

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 150){
+//             $('nav').addClass("sticky");
+//         }else{
+//             $('nav').removeClass("sticky");
+//         }
+//     });
+// });
